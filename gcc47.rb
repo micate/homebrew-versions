7175f82dfdce34eb10b158c19cba55b73b83effb
Mike McQuaid <mike@mikemcquaid.com>
2016-12-19 17:50:15 +0000

    gcc47: remove LLVM no-op code.

--- gcc47.rb
+++ gcc47.rb
@@ -64,8 +64,6 @@ class Gcc47 < Formula
   depends_on "cloog-ppl015"
   depends_on "ecj" if build.with?("java") || build.with?("all-languages")
 
-  fails_with :llvm
-
   # The bottles are built on systems with the CLT installed, and do not work
   # out of the box on Xcode-only systems due to an incorrect sysroot.
   def pour_bottle?

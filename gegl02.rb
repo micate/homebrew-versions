05cff9dbcd21ba091630df29b092ecd4f3716f60
Mike McQuaid <mike@mikemcquaid.com>
2016-12-19 17:42:18 +0000

    gegl02: remove ENV.no_optimiziation no-op.
    Closes #1490.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- gegl02.rb
+++ gegl02.rb
@@ -29,9 +29,6 @@ class Gegl02 < Formula
   keg_only "Older version of core gegl"
 
   def install
-    # ./configure breaks when optimization is enabled with llvm
-    ENV.no_optimization if ENV.compiler == :llvm
-
     args = %W[
       --disable-debug
       --disable-dependency-tracking

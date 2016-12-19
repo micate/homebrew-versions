b30c54ff1133b38c4f862781bdad70f4fe1d9760
Mike McQuaid <mike@mikemcquaid.com>
2016-12-19 17:50:15 +0000

    ruby187: remove LLVM no-op code.

--- ruby187.rb
+++ ruby187.rb
@@ -24,10 +24,6 @@ class Ruby187 < Formula
   depends_on "openssl"
   depends_on :x11 if build.with? "tcltk"
 
-  fails_with :llvm do
-    build 2326
-  end
-
   resource "rubygems" do
     url "https://rubygems.org/rubygems/rubygems-2.5.0.tgz"
     sha256 "a39e95acb86f52fdca38b1484b2eed61a7f76551119079d7a9f8c67c5be78831"
